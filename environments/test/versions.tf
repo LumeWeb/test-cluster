a27d5993c9fd183d945caad17478201493a4732b
Derrick Hammer <derrick@derrickhammer.com>
2024-11-14 10:32:58 -0500

    fix: add more skips

--- environments/test/versions.tf
+++ environments/test/versions.tf
@@ -3,5 +3,7 @@ terraform {
     key    = "test-cluster/terraform.tfstate"
     bucket = "lumeweb-test-cluster-devops"
     skip_credentials_validation = true
+    skip_region_validation = true
+    skip_metadata_api_check = true
   }
 }
