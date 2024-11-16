2ef65340ea1b50eca95c23752bed54273a515add
Derrick Hammer <derrick@derrickhammer.com>
2024-11-15 22:23:18 -0500

    refactor: record fqdn's

--- environments/test/outputs.tf
+++ environments/test/outputs.tf
@@ -12,3 +12,17 @@ output "autopilot_host" {
   description = "Host address of the autopilot node"
   value       = module.renterd_cluster.autopilot_host
 }
+output "bus_fqdn" {
+  description = "Fully Qualified Domain Name for the bus node"
+  value       = module.renterd_cluster.bus_dns_fqdn
+}
+
+output "worker_fqdns" {
+  description = "Fully Qualified Domain Names for the worker nodes"
+  value       = module.renterd_cluster.worker_dns_fqdns
+}
+
+output "autopilot_fqdn" {
+  description = "Fully Qualified Domain Name for the autopilot node"
+  value       = module.renterd_cluster.autopilot_dns_fqdn
+}
