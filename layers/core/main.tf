78047a6473271b812cafe384ce8017aa88ae5285
Derrick Hammer <derrick@derrickhammer.com>
2024-12-02 17:17:56 -0500

    delete

--- layers/core/main.tf
+++ layers/core/main.tf
@@ -1,7 +1,7 @@
-module "etcd" {
+/*module "etcd" {
   source = "git::https://github.com/LumeWeb/terraform-modules.git//modules/coordination/etcd?ref=develop"
 
 
   allowed_providers = var.allowed_providers
   root_password = "test-123"
-}
+}*/
