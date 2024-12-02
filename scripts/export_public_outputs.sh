8eafbbc814137658fb262a8bf266d6d9dc463b0f
Derrick Hammer <derrick@derrickhammer.com>
2024-12-01 23:01:22 -0500

    add check for TF_VAR_

--- scripts/export_public_outputs.sh
+++ scripts/export_public_outputs.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# Only export non-sensitive outputs
-cd "$TERRATEAM_ROOT"/"$TERRATEAM_DIR" && terraform output -json | \
-  jq -r 'to_entries |
-    map(select(.value.sensitive == false)) |
-    .[] |
-    "export \(.key)=\(.value.value)"'
+
+cd "$TERRATEAM_ROOT"/"$TERRATEAM_DIR" && \
+terraform output -json | \
+jq -r 'to_entries |
+  map(select(.value.sensitive == false)) |
+  .[] |
+  if (.key | startswith("TF_VAR_")) then
+    "export \(.key)=\(.value.value)"
+  else
+    "export TF_VAR_\(.key)=\(.value.value)"
+  end'
