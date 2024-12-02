c3c5e3b42703958443e350914ad5a11cfc7c2c4e
Derrick Hammer <derrick@derrickhammer.com>
2024-12-01 21:48:24 -0500

    move skip_ to versions.tf

--- scripts/s3_backend.sh
+++ scripts/s3_backend.sh
@@ -10,10 +10,5 @@ cat > backend.hcl << EOF
 bucket                      = "${BUCKET}"
 region                      = "${REGION}"
 key                         = "${KEY}"
-skip_credentials_validation = true
-skip_requesting_account_id  = true
-skip_metadata_api_check     = true
-skip_region_validation      = true
-skip_s3_checksum            = true
 EOF
 
