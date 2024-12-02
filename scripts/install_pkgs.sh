fabddef734aef9dc935d652c656b330b4921cc78
Derrick Hammer <derrick@derrickhammer.com>
2024-12-02 16:51:37 -0500

    use curl over apt

--- scripts/install_pkgs.sh
+++ scripts/install_pkgs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt-get update
-apt-get -y install software-properties-common
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CC86BB64
-add-apt-repository ppa:rmescandon/yq -y
+YQ_VERSION="v4.44.5"
+
+curl -L -o /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 \
+ && chmod +x /usr/local/bin/yq
 apt-get update
 apt-get install -y yq jq pass
