a25fdbb7aa662acee743ee2bb20c2d0c956f1324
Derrick Hammer <derrick@derrickhammer.com>
2024-12-02 16:51:47 -0500

    use curl over apt

--- scripts/install_pkgs.sh
+++ scripts/install_pkgs.sh
@@ -5,4 +5,4 @@ YQ_VERSION="v4.44.5"
 curl -L -o /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 \
  && chmod +x /usr/local/bin/yq
 apt-get update
-apt-get install -y yq jq pass
+apt-get install -y jq pass
