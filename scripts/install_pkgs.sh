e0739b8933590c49da81908c85365ec15c073d55
Derrick Hammer <derrick@derrickhammer.com>
2024-12-02 16:48:44 -0500

    need to install yq

--- scripts/install_pkgs.sh
+++ scripts/install_pkgs.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 apt-get update
+apt-get -y install software-properties-common
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CC86BB64
+add-apt-repository ppa:rmescandon/yq -y
+apt-get update
 apt-get install -y yq jq pass
