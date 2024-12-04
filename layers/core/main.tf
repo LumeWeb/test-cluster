9a924f88aa33222d37baea3df715a453507073ed
Derrick Hammer <derrick@derrickhammer.com>
2024-12-03 21:59:09 -0500

    delete renterd

--- layers/core/main.tf
+++ layers/core/main.tf
@@ -54,7 +54,7 @@ module "mysql" {
   }
 }
 
-module "renterd" {
+/*module "renterd" {
   source = "git::https://github.com/LumeWeb/terraform-modules.git//modules/compute/renterd?ref=develop"
 
   allowed_providers = var.allowed_providers
@@ -70,6 +70,7 @@ module "renterd" {
   }
 
   network = {
+    http_port = 80
     enable_ssl = false
   }
 
@@ -99,4 +100,4 @@ module "renterd" {
   }
 
   depends_on = [module.mysql]
-}
+}*/
