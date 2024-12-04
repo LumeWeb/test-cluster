188632ab06b49291017246514328e9dc89654e11
Derrick Hammer <derrick@derrickhammer.com>
2024-12-03 23:51:57 -0500

    add s3 record

--- layers/core/dns.tf
+++ layers/core/dns.tf
@@ -5,3 +5,11 @@ resource "cloudflare_dns_record" "renterd" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_dns_record" "renterd_s3" {
+  zone_id = var.domain_zone_id
+  name    = module.renterd.s3_fqdn
+  content = module.renterd.provider_host
+  type    = "CNAME"
+  ttl     = 1
+}
