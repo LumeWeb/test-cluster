e6db17990b55ec5bbc0d490418785c05d7bf62f3
Derrick Hammer <derrick@derrickhammer.com>
2024-12-04 00:54:59 -0500

    delete mysql

--- layers/core/outputs.tf
+++ layers/core/outputs.tf
@@ -2,12 +2,12 @@ output "etcd_endpoint" {
   description = "ETCD service endpoint"
   value = module.etcd.service.endpoints[0]
 }
-
+/*
 output "mysql_endpoint" {
   description = "MySQL service endpoint"
   value = "${module.mysql.provider_host}:${module.mysql.port}"
 }
-
+*/
 /*output "renterd_endpoint" {
   description = "Renterd service endpoint"
   value = module.renterd.dns_fqdn
