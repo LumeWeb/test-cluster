4e3b2f776e866955ad3f02605478eaaa493fc2b5
Derrick Hammer <derrick@derrickhammer.com>
2024-12-05 21:41:34 -0500

    initial services layer

--- layers/services/main.tf
+++ layers/services/main.tf
@@ -1,17 +1,20 @@
+locals {
+  base_domain = "test-svc.node.testing.pinner.xyz"
+}
+
 # MySQL Cluster
 module "mysql_cluster" {
   source = "git::https://github.com/LumeWeb/terraform-modules.git//modules/db/mysql-cluster?ref=develop"
 
-  cluster_name    = "mysql-${var.environment}"
-  environment     = var.environment
-  replica_count   = 1  # 1 replica = 2 total nodes
-  
-  root_password   = var.mysql_root_password
-  repl_password   = var.mysql_repl_password
+  environment  = var.environment
+  replica_count = 1  # 1 replica = 2 total nodes
+
+  root_password = var.mysql_root_password
+  repl_password = var.mysql_repl_password
 
   # ETCD Configuration
   etc_endpoints = [var.etcd_endpoint]
-  
+
   # Resource Configuration
   master_resources = {
     cpu_units    = 2
@@ -32,9 +35,8 @@ module "mysql_cluster" {
 module "proxysql" {
   source = "git::https://github.com/LumeWeb/terraform-modules.git//modules/db/proxysql?ref=develop"
 
-  name        = "proxysql-${var.environment}"
   environment = var.environment
-  
+
   admin_password = var.proxysql_admin_password
 
   # Connect to ETCD
@@ -66,30 +68,30 @@ module "proxysql" {
 module "renterd_cluster" {
   source = "git::https://github.com/LumeWeb/terraform-modules.git//modules/compute/renterd-cluster?ref=develop"
 
-  environment = var.environment
+  environment = "dev"
   base_domain = var.base_domain
-  
+
   # Authentication
-  seed = var.renterd_seed
-  bus_api_password = var.renterd_api_password
+  seed                = var.renterd_seed
+  bus_api_password    = var.renterd_api_password
   worker_api_password = var.renterd_api_password
   metrics_password = var.renterd_metrics_password
 
   # MySQL Configuration via ProxySQL
   database = {
-    type = "mysql"
-    mysql_uri = "mysql://${module.proxysql.provider_host}:${module.proxysql.port}"
+    type           = "mysql"
+    mysql_uri = format("%s:%d", module.proxysql.provider_host, module.proxysql.port)
     mysql_password = var.mysql_root_password
   }
 
   # Resource Configuration
-  bus_cpu_cores = 2
-  bus_memory_size = 4
+  bus_cpu_cores    = 2
+  bus_memory_size  = 4
   bus_storage_size = 100
-  
-  worker_cpu_cores = 2
+
+  worker_cpu_cores   = 2
   worker_memory_size = 4
-  worker_count = 2
+  worker_count       = 1
 
   allowed_providers = var.allowed_providers
 
