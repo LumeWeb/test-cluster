8d9e5559bb2e9fbf5edb491a33e0091fa8ca903a
Derrick Hammer <derrick@derrickhammer.com>
2024-12-07 00:45:37 -0500

    use_path_style

--- layers/services/versions.tf
+++ layers/services/versions.tf
@@ -38,6 +38,6 @@ data "terraform_remote_state" "remote_states" {
     skip_region_validation     = true
     skip_metadata_api_check    = true
     skip_s3_checksum          = true
-    force_path_style          = true
+    use_path_style          = true
   }
 }
