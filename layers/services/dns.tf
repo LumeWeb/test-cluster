21c9f8aab800a1fe54d4474d9fd8ce57340548f0
Derrick Hammer <derrick@derrickhammer.com>
2024-12-07 01:02:10 -0500

    provider_host -> host

--- layers/services/dns.tf
+++ layers/services/dns.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_dns_record" "renterd_bus" {
   zone_id = var.domain_zone_id
   name    = module.renterd_cluster.bus.dns_fqdn
-  content = module.renterd_cluster.bus.provider_host
+  content = module.renterd_cluster.bus.host
   type    = "CNAME"
   ttl     = 1
 }
@@ -9,7 +9,7 @@ resource "cloudflare_dns_record" "renterd_bus" {
 resource "cloudflare_dns_record" "renterd_autopilot" {
   zone_id = var.domain_zone_id
   name    = module.renterd_cluster.autopilot.dns_fqdn
-  content = module.renterd_cluster.autopilot.provider_host
+  content = module.renterd_cluster.autopilot.host
   type    = "CNAME"
   ttl     = 1
 }
@@ -18,7 +18,7 @@ resource "cloudflare_dns_record" "renterd_workers" {
   count = length(module.renterd_cluster.workers)
   zone_id = var.domain_zone_id
   name    = module.renterd_cluster.workers[count.index].dns_fqdn
-  content = module.renterd_cluster.workers[count.index].provider_host
+  content = module.renterd_cluster.workers[count.index].host
   type    = "CNAME"
   ttl     = 1
 }
