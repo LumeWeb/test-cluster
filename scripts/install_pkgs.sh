6a2fa8753f62af69129f6ab4ea139d2f0bac4276
Derrick Hammer <derrick@derrickhammer.com>
2024-12-13 23:52:20 -0500

    move back to digger

--- scripts/install_pkgs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-YQ_VERSION="v4.44.5"
-
-curl -L -o /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 \
- && chmod +x /usr/local/bin/yq
-apt-get update
-apt-get install -y jq pass
